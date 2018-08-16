c68e88622314e51498939c8692aab92be69b102f
David Colucci <david_colucci@condenast.com>
2018-08-16 17:10:03 -0400

    more fun

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,19 +2,46 @@ class Point(x: Int, y: Int) {
   override def toString(): String = "(" + x + ", " + y + ")"
 }
 
-class Util {
+object Util {
   def maybeItWillReturnSomething(flag: Boolean): Option[String] = {
     if (flag) Some("Here's your f'n string") else None
+  }
+
+  def matcher(input: Option[String]): String = {
+    val default = "[EMPTY]"
+    input match {
+      case Some(value) => value
+      case None => default
+    }
+  }
+
+  def getAnInt(input: Option[Int]): Int = {
+    input.getOrElse(5)
+  }
+
+  def mapper(input: Option[Int]): Option[Int] = {
+    input map(_ * 2)
+  }
+
+  def folder(input: Option[Int]): Int = {
+    // question, how to express an identify / noop func?
+    // input.fold(1)(_)
+    input.fold(1)(_ * 3)
+  }
+
+  val randInt: () => Int = {
+    val result = util.Random.nextInt
+    () => result
   }
 }
 
 object Main extends App {
-  val util = new Util()
-  val result1 = util.maybeItWillReturnSomething(true)
-  val result2 = util.maybeItWillReturnSomething(false)
-  val finalResult: String = result1 getOrElse "4bogus"
-  println(finalResult)
-  // println(result2)
+  val result1 = Util.randInt()
+  val result2 = Util.randInt()
+  val result3 = Util.randInt()
+  println(result1)
+  println(result2)
+  println(result3)
 }
 
 
