e0affb2a68b2d97590e1537be0ea3eeb0d954ce2
David Colucci <davidanthonycolucci@gmail.com>
2018-08-16 08:22:18 -0400

    a bunch of other bogus stuff

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,21 @@
-object Main extends App {
-  println("beginning...")
-  assert(true == false)
-  println("finished!")
+class Point(x: Int, y: Int) {
+  override def toString(): String = "(" + x + ", " + y + ")"
 }
+
+class Util {
+  def maybeItWillReturnSomething(flag: Boolean): Option[String] = {
+    if (flag) Some("Here's your f'n string") else None
+  }
+}
+
+object Main extends App {
+  val util = new Util()
+  val result1 = util.maybeItWillReturnSomething(true)
+  val result2 = util.maybeItWillReturnSomething(false)
+  val finalResult: String = result1 getOrElse "4bogus"
+  println(finalResult)
+  // println(result2)
+}
+
+
+// https://www.scala-exercises.org/std_lib/options
